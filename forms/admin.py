3f23dc335c662c247749a79f4cb1d13368b9914e
Eugene600 <ugowach160@gmail.com>
2024-07-02 21:59:34 +0300

    Updated models page I have an issue though when adding another question to a form

--- forms/admin.py
+++ forms/admin.py
@@ -1,44 +1,58 @@
-# from django import forms
-# from django.contrib import admin
-# # from .models import Question, Option, QuestionType, ShortResponse, LongResponse, MultipleChoiceResponse, CheckboxResponse
+from django.contrib import admin
+from .models import Form, Question, Option, Response
 
-# # class OptionInline(admin.TabularInline):
-# #     model = Option
-# #     extra = 1
+#username: admin
+#password: daystar123 
 
-# # class QuestionAdminForm(forms.ModelForm):
-# #     class Meta:
-# #         model = Question
-# #         fields = '__all__'
-    
-# #     def __init__(self, *args, **kwargs):
-# #         super().__init__(*args, **kwargs)
-# #         question_type = self.instance.question_type
-# #         if question_type in [QuestionType.MULTIPLE_CHOICES, QuestionType.CHECKBOXES]:
-# #             self.fields['options'] = forms.ModelMultipleChoiceField(
-# #                 queryset=Option.objects.filter(question=self.instance),
-# #                 widget=forms.CheckboxSelectMultiple
-# #             )
-# #         elif question_type == QuestionType.SHORT_PARAGRAPH:
-# #             self.fields['response_text'] = forms.CharField(max_length=255)
-# #         elif question_type == QuestionType.LONG_PARAGRAPH:
-# #             self.fields['response_text'] = forms.CharField(widget=forms.Textarea)
+# class OptionInline(admin.TabularInline):
+#     model = Question.option_values.through
+#     extra = 1
 
-# # class QuestionAdmin(admin.ModelAdmin):
-# #     form = QuestionAdminForm
-# #     inlines = [OptionInline]
-# #     list_display = ('question_text', 'question_type')
-    
-# #     def save_formset(self, request, form, formset, change):
-# #         instances = formset.save(commit=False)
-# #         for instance in instances:
-# #             instance.question = form.instance
-# #             instance.save()
-# #         formset.save_m2m()
+# @admin.register(Form)
+# class FormAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'department','contact','created_at']
+#     search_fields = ['name','department']
 
+# @admin.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     form = QuestionAdminForm
+#     list_display = ['question_text','question_type','form']
+#     inlines = [OptionInline]
+
+#     class Media:
+#         js = ('templates/admin.js')
+
+# @admin.register(Option)
+# class OptionAdmin(admin.ModelAdmin):
+#     list_display = ['option_text']     
+
+# @admin.register(Response)
+# class ResponseAdmin(admin.ModelAdmin):
+#     list_display = ['question', 'short_response_text','long_response_text','multiple_choice_option']               
+
+# admin.site.register(Form)
 # admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Option)
-# admin.site.register(ShortResponse)
-# admin.site.register(LongResponse)
-# admin.site.register(MultipleChoiceResponse)
-# admin.site.register(CheckboxResponse)
+# admin.site.register(Response)
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 1
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class FormAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+admin.site.register(Form, FormAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Option)
+admin.site.register(Response)
+
+
+
--- forms/models.py
+++ forms/models.py
@@ -1,10 +1,16 @@
+from django import forms
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 class Form(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     department = models.CharField(max_length=200)
-    contact = models.CharField(max_length=10)
+    contact = models.CharField(
+        max_length=10,     
+        validators=[MinLengthValidator(10)],
+        )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -17,41 +23,59 @@ class QuestionType(models.TextChoices):
     CHECKBOXES = 'CHECKBOX', 'Checkboxes'
 
 class Question(models.Model):
-    form = models.ForeignKey(Form, on_delete=models.CASCADE)
-    question_text = models.CharField(max_length=500)
+    FORMATS = (
+        ('short_answer', 'Short Answer'),
+        ('long_answer', 'Long Answer'),
+        ('single_choice', 'Single Choice'),
+        ('multiple_choice', 'Multiple Choice'),
+    )
+    form = models.ForeignKey(Form,related_name='questions' ,on_delete=models.CASCADE)
+    question_text = models.TextField()
     question_type = models.CharField(
-        max_length=10,
-        choices=QuestionType.choices,
-        default=QuestionType.SHORT_PARAGRAPH
+        max_length=20,
+        choices=FORMATS
     )
     # Fields for options
-    option_text = models.CharField(max_length=255, blank=True, null=True)
-    option_values = models.ManyToManyField('Option', blank=True)
 
     def __str__(self):
         return self.question_text
 
 class Option(models.Model):
-    option_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, related_name='options',on_delete=models.CASCADE)
+    option_text = models.CharField(max_length=255)
 
     def __str__(self):
         return self.option_text
 
 class Response(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    # Response data
-    short_response_text = models.CharField(max_length=255, blank=True, null=True)
-    long_response_text = models.TextField(blank=True, null=True)
-    multiple_choice_option = models.ForeignKey(Option, blank=True, null=True, on_delete=models.CASCADE, related_name='multiple_choice_responses')
-    checkbox_options = models.ManyToManyField(Option, blank=True, related_name='checkbox_responses')
+    option = models.ForeignKey(Option, null=True, blank=True, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    #will add user later
+    
+    
+#This is a custom admin forms to handle the conditional display of fields based on the question type selected
+#Learning purposes, to be deleted later
+# class QuestionAdminForm(forms.ModelForm):
+#     class Meta:
+#         model = Question
+#         fields = ['form', 'question_text', 'question_type', 'option_text', 'option_values']
+#         #Just learnt about these two, will use later
+#         # labels = {"first_name": "First Name","last_name": "Last Name",}
+#         # widgets = {"first_name": forms.TextInput(attrs={"class": "my-class"}),"last_name": forms.Textarea(attrs={"rows": 4, "cols": 15}),}
 
-    def get_response(self):
-        if self.question.question_type == QuestionType.SHORT_PARAGRAPH:
-            return self.short_response_text
-        elif self.question.question_type == QuestionType.LONG_PARAGRAPH:
-            return self.long_response_text
-        elif self.question.question_type == QuestionType.MULTIPLE_CHOICES:
-            return self.multiple_choice_option
-        elif self.question.question_type == QuestionType.CHECKBOXES:
-            return self.checkbox_options.all()
-        return None
+
+#         def __init__(self, *args, **kwargs):
+#             super().__init__(*args,**kwargs)
+
+#             self.fields['option_text'].widget = forms.HiddenInput()
+#             self.fields['option_values'].widget = forms.HiddenInput()
+
+#             if self.instance.pk:
+#                 if self.instance.question_type in [QuestionType.MULTIPLE_CHOICES, QuestionType.CHECKBOXES]:
+#                     self.fields['option_values'].widget = forms.CheckboxSelectMultiple()
+#                 else:
+#                     self.fields['option_text'].widget = forms.HiddenInput()
+#                     self.fields['option_values'].widget = forms.HiddenInput()
+
